6613709710c626d45dd9bde043210ce0ff43d5a3
Rakesh Patel <emailme@rakeshrpatel.com>
2016-09-20 10:27:30 -0400

    update

--- index.js
+++ index.js
@@ -9,24 +9,24 @@
 
 // 'use strict'
 
-var 
-	express = require('express'),
+var
+    express = require('express'),
     router = express.Router(),
-	mongodb = require('mongodb'),
+    mongodb = require('mongodb'),
     mongoose = require('mongoose'),
-	Schema = mongoose.Schema,
-	bodyParser = require('body-parser'),
-	Request = require('request'),
-	port = process.env.PORT || 8080,
-	app = express(),
+    Schema = mongoose.Schema,
+    bodyParser = require('body-parser'),
+    Request = require('request'),
+    port = process.env.PORT || 8080,
+    app = express(),
     ObjectID = mongodb.ObjectID,
     Heroku_URI = 'https://salty-chamber-30914.herokuapp.com',
     // MONGOLAB_URI = "mongodb://Rakeshpatel87p:Printer1@ds035846.mlab.com:35846/coffeedrinkinfo",
     db;
-	
+
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))	
-// parse application/json
+app.use(bodyParser.urlencoded({ extended: false }))
+    // parse application/json
 app.use(bodyParser.json())
 
 app.use(express.static('public'));
@@ -34,8 +34,8 @@ app.use(express.static('public'));
 // app.set('port', (process.env.PORT || 8080))
 
 // Connect to the database before starting the application server.
-mongoose.connect(process.env.MONGOLAB_URI, function(err, database){
-    if (err){
+mongoose.connect(process.env.MONGOLAB_URI, function(err, database) {
+    if (err) {
         console.log(err);
         process.exit(1);
     }
@@ -43,7 +43,7 @@ mongoose.connect(process.env.MONGOLAB_URI, function(err, database){
     db = database;
     console.log('Database connection ready captain')
 
-    var server = app.listen(process.env.PORT || 8080, function(){
+    var server = app.listen(process.env.PORT || 8080, function() {
         var port = server.address().port;
         console.log('App now running on port', port)
     });
@@ -56,8 +56,8 @@ mongoose.connect(process.env.MONGOLAB_URI, function(err, database){
 
 
 var CoffeeDrinkSchema = new mongoose.Schema({
-	name: {type: String, unique: true},
-	price: {type: Number},
+    name: { type: String, unique: true },
+    price: { type: Number },
 });
 
 // For accessing /webhook locally??
@@ -79,7 +79,7 @@ app.get('/webhook', function(req, res) {
         res.send(req.query['hub.challenge'])
     }
     res.send('Error, wrong token');
-    sendOrderMessage();
+    sendMenuMessage();
 })
 
 // to post data
@@ -91,7 +91,7 @@ app.post('/webhook', function(req, res) {
         if (event.message && event.message.text) {
             var text = event.message.text
             if (text === 'order') {
-                sendOrderMessage(sender)
+                sendMenuMessage(sender)
                 continue
             }
             // sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
@@ -99,44 +99,39 @@ app.post('/webhook', function(req, res) {
         if (event.postback) {
             var text = JSON.stringify(event.postback)
             console.log('this is event postback------------', event.postback.payload)
-            // sendTextMessage(sender, "Postback received: " + text.substring(0, 200), token)
-            // continue
+                // sendTextMessage(sender, "Postback received: " + text.substring(0, 200), token)
+                // continue
 
-            if (event.postback.payload === 'House_Coffee'){
-            	CoffeeDrink.findOne({name: event.postback.payload}, function(err, coffeeDrink){
-                    if (err){
-                        console.log(err)
-                    }
-                    console.log('this is the coffee drink-----------', coffeeDrink);
-                })
-                // Request
-                //  .get('/drinkInfo/' + event.postback.payload)
-            	// 	.on('response', function(coffeeDrink){
-            	// 		console.log('this is the response----------', coffeeDrink)
-            	// 	});
-            	sendTextMessage(sender, 'Testing');
+            if (event.postback.payload === 'House_Coffee') {
+                CoffeeDrink.findOne({ name: event.postback.payload }, function(err, coffeeDrink) {
+                        if (err) {
+                            console.log(err)
+                        }
+                        orderSummaryMessage(sender, coffeeDrink)
+                    })
+                    // sendTextMessage(sender, 'Testing');
             }
         }
     }
     res.sendStatus(200)
 });
 
-app.post('/drinkInfo', function(req, res){
-	 CoffeeDrink.create({name: req.body.drinkName, price: req.body.price}, function(err, newDrinkEntry){
-	 	if (err) {
-	 		res.status(500).json(err)
-	 	}; 
-	 	res.status(201).json(newDrinkEntry);
-	 });
+app.post('/drinkInfo', function(req, res) {
+    CoffeeDrink.create({ name: req.body.drinkName, price: req.body.price }, function(err, newDrinkEntry) {
+        if (err) {
+            res.status(500).json(err)
+        };
+        res.status(201).json(newDrinkEntry);
+    });
 });
 
-app.get('/drinkInfo/:drink', function(req, res){
-	CoffeeDrink.findOne({name: req.params.drink}, function(err, coffeeDrink){
-		if (err) {
-			res.status(500).json(err);
-		}; 
-		res.status(201).json(coffeeDrink)
-	});
+app.get('/drinkInfo/:drink', function(req, res) {
+    CoffeeDrink.findOne({ name: req.params.drink }, function(err, coffeeDrink) {
+        if (err) {
+            res.status(500).json(err);
+        };
+        res.status(201).json(coffeeDrink)
+    });
 });
 
 
@@ -163,68 +158,68 @@ function sendTextMessage(sender, text) {
     })
 }
 
-function sendOrderMessage(sender) {
+function sendMenuMessage(sender) {
     var messageData = {
         "attachment": {
             "type": "template",
             "payload": {
                 "template_type": "generic",
                 "elements": [{
-                    "title": "House Coffee",
-                    "subtitle": "Freshly brewed, brought to you from la montanas de Honduras. $3.00",
-                    "image_url": "http://wtop.com/wp-content/uploads/2015/03/getty_030315_coffee.jpg",
-                    "buttons": [{
-                        "type": "postback",
-                        "title": "Order Me!",
-                        "payload": "House_Coffee"
+                        "title": "House Coffee",
+                        "subtitle": "Freshly brewed, brought to you from la montanas de Honduras. $3.00",
+                        "image_url": "http://wtop.com/wp-content/uploads/2015/03/getty_030315_coffee.jpg",
+                        "buttons": [{
+                            "type": "postback",
+                            "title": "Order Me!",
+                            "payload": "House_Coffee"
 
+                        }, {
+                            "type": "postback",
+                            "title": "Checkout",
+                            "payload": "Check out"
+                        }],
+                    },
+
+                    {
+                        "title": "Cappuccino",
+                        "subtitle": "Italian Coffee drink, prepared with 2 shots of espresso, hot milk, and steamed milk foam. (Let drooling commence) $4.00.",
+                        "image_url": "http://del.h-cdn.co/assets/15/45/980x490/landscape-1446486666-giulia-mule.jpg",
+                        "buttons": [{
+                            "type": "postback",
+                            "title": "Order Me!",
+                            "payload": "Cappuccino"
+
+                        }, {
+                            "type": "postback",
+                            "title": "Checkout",
+                            "payload": "Checkout"
+                        }],
+                    },
+
+                    {
+                        "title": "Cortado",
+                        "subtitle": "Equal parts espresso and steamed milk. 5 ounches total volume. We got you! $3.00",
+                        "image_url": "https://upload.wikimedia.org/wikipedia/commons/1/16/Caf%C3%A9Cortado(Tallat).jpg",
+                        "buttons": [{
+                            "type": "postback",
+                            "title": "Order Me!",
+                            "payload": "Cortado",
+
+                        }, {
+                            "type": "postback",
+                            "title": "Checkout",
+                            "payload": "Checkout",
+                        }],
                     }, {
-                        "type": "postback",
-                        "title": "Checkout",
-                        "payload": "Check out"
-                    }],
-                },
-
-                {
-                    "title": "Cappuccino",
-                    "subtitle": "Italian Coffee drink, prepared with 2 shots of espresso, hot milk, and steamed milk foam. (Let drooling commence) $4.00.",
-                    "image_url": "http://del.h-cdn.co/assets/15/45/980x490/landscape-1446486666-giulia-mule.jpg",
-                    "buttons": [{
-                        "type": "postback",
-                        "title": "Order Me!",
-                        "payload": "Cappuccino"
-
-                    }, {
-                        "type": "postback",
-                        "title": "Checkout",
-                        "payload": "Checkout"
-                    }],
-                },
-
-                {
-                    "title": "Cortado",
-                    "subtitle": "Equal parts espresso and steamed milk. 5 ounches total volume. We got you! $3.00",
-                    "image_url": "https://upload.wikimedia.org/wikipedia/commons/1/16/Caf%C3%A9Cortado(Tallat).jpg",
-                    "buttons": [{
-                        "type": "postback",
-                        "title": "Order Me!",
-                        "payload": "Cortado",
-
-                    }, {
-                        "type": "postback",
-                        "title": "Checkout",
-                        "payload": "Checkout",
-                    }],
-                },
-                {
-                	"title": "In Limited Release",
-                	"subtitle": "If we dont have your drink, you'll have to use our beautiful register",
-                	"buttons": [{
-                		"type": "postback",
-                		"title": "Click Me If You Like this Feature!",
-                		"payload": "Other users"
-                	}],
-                }]
+                        "title": "In Limited Release",
+                        "subtitle": "If we dont have your drink, you'll have to use our beautiful register",
+                        "buttons": [{
+                            "type": "postback",
+                            "title": "Click Me If You Like this Feature!",
+                            "payload": "Other users"
+                        }],
+                    }
+                ]
             }
         }
     };
@@ -236,6 +231,25 @@ function sendOrderMessage(sender) {
         json: {
             recipient: { id: sender },
             message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
+
+function orderSummaryMessage(sender, coffeeDrink) {
+    // var messageData = { text: text }
+    Request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: { access_token: token },
+        method: 'POST',
+        json: {
+            recipient: { id: sender },
+            message: coffeeDrink.name,
         }
     }, function(error, response, body) {
         if (error) {
